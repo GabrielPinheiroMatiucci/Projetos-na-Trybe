709e313295b1306c9c469c72c749681548b1bc85
Semnome1505 <Gabriel_1505@outlook.com>
2022-07-12 22:02:23 -0300

    test: add test validating non palindromes.

--- project-algorithms/tests/test_palindromes_recursive.py
+++ project-algorithms/tests/test_palindromes_recursive.py
@@ -4,3 +4,11 @@ from challenges.challenge_palindromes_recursive import is_palindrome_recursive
 def test_string_vazia():
     text = ''
     assert is_palindrome_recursive(text, 0, (len(text) - 1)) is False
+
+
+def test_string_nao_e_palindromo():
+    text = 'alo'
+    assert is_palindrome_recursive(text, 0, (len(text) - 1)) is False
+
+    text = 'abab'
+    assert is_palindrome_recursive(text, 0, (len(text) - 1)) is False
