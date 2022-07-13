6ebb7ee53c3fbbc54d6387f66439153573c54441
Semnome1505 <Gabriel_1505@outlook.com>
2022-07-12 22:04:38 -0300

    test: add test validating palindromes.

--- project-algorithms/tests/test_palindromes_recursive.py
+++ project-algorithms/tests/test_palindromes_recursive.py
@@ -12,3 +12,17 @@ def test_string_nao_e_palindromo():
 
     text = 'abab'
     assert is_palindrome_recursive(text, 0, (len(text) - 1)) is False
+
+
+def test_string_e_palindromo():
+    text = 'a'
+    assert is_palindrome_recursive(text, 0, (len(text) - 1)) is True
+
+    text = 'aa'
+    assert is_palindrome_recursive(text, 0, (len(text) - 1)) is True
+
+    text = 'arara'
+    assert is_palindrome_recursive(text, 0, (len(text) - 1)) is True
+
+    text = 'TENET'
+    assert is_palindrome_recursive(text, 0, (len(text) - 1)) is True
