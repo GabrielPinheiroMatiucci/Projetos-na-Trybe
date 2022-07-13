0c92a55e19cb221cd086151c6d9220fec9612ed7
Semnome1505 <Gabriel_1505@outlook.com>
2022-07-12 21:07:03 -0300

    test: add tests validating anagrams case insensitive.

--- project-algorithms/tests/test_anagrams.py
+++ project-algorithms/tests/test_anagrams.py
@@ -1,7 +1,7 @@
 from challenges.challenge_anagrams import is_anagram
 
 
-def test_palavras_nao_sao_anagramas():
+def test_palavras_nao_sao_anagramas_e_palavras_vazias():
     first_string = 'teste'
     second_string = ''
     assert is_anagram(first_string, second_string) is False
@@ -17,3 +17,13 @@ def test_palavras_nao_sao_anagramas():
     first_string = 'ola'
     second_string = 'lhao'
     assert is_anagram(first_string, second_string) is False
+
+
+def test_palavras_case_insensitive():
+    first_string = 'olha'
+    second_string = 'LAOH'
+    assert is_anagram(first_string, second_string) is True
+
+    first_string = 'PaPeL'
+    second_string = 'AElpp'
+    assert is_anagram(first_string, second_string) is True
