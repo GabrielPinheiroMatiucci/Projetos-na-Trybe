6bc92da0e21a98af139f052bdc68ae12ecd108eb
Semnome1505 <Gabriel_1505@outlook.com>
2022-07-12 21:56:22 -0300

    test: add test validating valid permanence periods and target times.

--- project-algorithms/tests/test_study_schedule.py
+++ project-algorithms/tests/test_study_schedule.py
@@ -15,3 +15,19 @@ def test_target_time_vazio():
     permanence_periods = [(1, 5), (2, 8)]
     target_time = None
     assert study_schedule(permanence_periods, target_time) is None
+
+
+def test_permanence_periods_e_target_time_validos():
+    permanence_periods = [(1, 5), (2, 8), (3, 3), (2, 7)]
+
+    target_time = 1
+    assert study_schedule(permanence_periods, target_time) == 1
+
+    target_time = 2
+    assert study_schedule(permanence_periods, target_time) == 3
+
+    target_time = 5
+    assert study_schedule(permanence_periods, target_time) == 3
+
+    target_time = 9
+    assert study_schedule(permanence_periods, target_time) == 0
