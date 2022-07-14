c30203933b288d472ecb5acc575c726c2008d36a
Semnome1505 <Gabriel_1505@outlook.com>
2022-07-14 14:42:30 -0300

    feat: add tests validating search.

--- trybe-is-not-google/tests/test_queue.py
+++ trybe-is-not-google/tests/test_queue.py
@@ -1,3 +1,4 @@
+import pytest
 from ting_file_management.queue import Queue
 from ting_file_management.file_management import txt_importer
 
@@ -26,3 +27,18 @@ def test_dequeue():
 
     queue.dequeue()
     assert len(queue._queue) == 0
+
+
+def test_search():
+    queue = Queue()
+
+    queue.enqueue(True)
+    queue.enqueue(False)
+    queue.enqueue(None)
+
+    assert queue.search(0) is True
+    assert queue.search(1) is False
+    assert queue.search(2) is None
+
+    with pytest.raises(IndexError):
+        queue.search(3)
